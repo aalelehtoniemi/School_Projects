c2886b57124af6c576b2fdf8bd6ca941b3e5bffd
Aale Lehtoniemi <aalehton@edu.lapinamk.fi>
2022-03-11 13:55:18 +0200

    Kaikki kunnossa

--- aale_lehtoniemi_full_stack-projekti/kayttoliittymasuunnitelma/admin/randomstring.php
+++ aale_lehtoniemi_full_stack-projekti/kayttoliittymasuunnitelma/admin/randomstring.php
@@ -2,8 +2,8 @@
 /*
 	file: 	randomstring.php
 	desc:	Tekee 10 merkkiä pitkän satunnaisen tekstin valituista merkeistä
-	date:	14.5.2018
-	auth:	Yrjö K
+	date:	14.5.2020
+	auth:	Aale L
 */
 $length = 10;
 $randomString = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
